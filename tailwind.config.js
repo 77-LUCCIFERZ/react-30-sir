6e8f1a26c2e9a471096ba4046654abd4d4ff795c
Sandesh Bhattarai <sandesh.bhattarai@kotuko.it>
2024-08-05 08:51:00 +0545

    React code all

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const flowbite = require("flowbite-react/tailwind")
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    flowbite.content(),
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
